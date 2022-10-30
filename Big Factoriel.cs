c72e66ee911351b052eef394b1ae81d41e49b540
Nikolay Angelov <108483720+nikolaygangelov@users.noreply.github.com>
2022-10-30 15:55:33 +0200

    Add files via upload

---
+++ Big Factoriel.cs
@@ -0,0 +1,24 @@
+﻿
+
+namespace _2._Big_Factorial
+{
+    using System;
+    using System.Linq;
+    using System.Numerics;
+    using System.Collections.Generic;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int N = int.Parse(Console.ReadLine());
+            BigInteger f = 1;
+
+            for (int i = 2; i <= N; i++)
+            {
+                f *= i;
+            }
+            Console.WriteLine(f); 
+        }
+    }
+}
