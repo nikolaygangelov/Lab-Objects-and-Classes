111ea587033ff4925715f55bc353a68d7878044c
Nikolay Angelov <108483720+nikolaygangelov@users.noreply.github.com>
2022-10-30 15:55:10 +0200

    Add files via upload

---
+++ Randomize Words.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using System.Numerics;
+using System.Collections.Generic;
+
+namespace _1._Randomize_Words
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> words = Console.ReadLine()
+                .Split()
+                .ToList();
+
+            Random random = new Random();
+            for (int i = 0; i < words.Count; i++)
+            {
+                
+                string firstWord = words[i];
+                int randomIndex = random.Next(0, words.Count);
+                string nextWord = words[randomIndex];
+
+                words[i] = nextWord;
+                words[randomIndex] = firstWord;
+            }
+            Console.WriteLine(string.Join(Environment.NewLine, words));
+        }
+    }
+
+    
+}
