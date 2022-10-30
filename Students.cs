e686fd39149d5ad0577272472141015fef24eb6a
Nikolay Angelov <108483720+nikolaygangelov@users.noreply.github.com>
2022-10-30 15:56:28 +0200

    Add files via upload

---
+++ Students.cs
@@ -0,0 +1,57 @@
+﻿
+
+namespace _4._Students
+{
+    using System;
+    using System.Linq;
+    using System.Numerics;
+    using System.Collections.Generic;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Student> studentsList = new List<Student>();
+
+            string command;
+            while ((command = Console.ReadLine()) != "end")
+            {
+                string[] inputParameters = command.Split();
+                string firstName = inputParameters[0];
+                string lastName = inputParameters[1];
+                int age = int.Parse(inputParameters[2]);
+                string homeTown = inputParameters[3];
+
+                Student student = new Student(firstName, lastName, age, homeTown);
+                studentsList.Add(student);
+            }
+
+            string cityName = Console.ReadLine();
+
+            List<Student> filteredStudentsList = studentsList
+                .Where(student => student.HomeTown == cityName)
+                .ToList();
+
+            foreach (Student student in filteredStudentsList)
+            {
+                Console.WriteLine($"{student.FirstName} {student.LastName} is {student.Age} years old.");
+            }
+
+        }
+    }
+
+    public class Student
+    {
+        public Student(string firstName, string lastName, int age, string homeTown)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+            HomeTown = homeTown;
+        }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string HomeTown { get; set; }
+    }
+}
